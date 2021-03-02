2590bc6cfc5c487898cc30cb317bff149c24c212
Anton <78906032+AntoniKR@users.noreply.github.com>
2021-03-02 11:11:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    int n=1;
-    for (int i=1;i<b;i++)
+    int n = 1;
+    for (int i = 1; i < b; i++)
     {
-        if (a%i==0 && b%i==0)
-            n=i;
+        if (a % i == 0 && b % i == 0)
+            n = i;
     }
     return n;
 }
